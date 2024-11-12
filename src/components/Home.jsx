a6369c5149e715d66247fa42c295355d3ca76d43
Icaro Jordano <Icarojordano>
2024-11-12 13:32:19 -0300

    feat(Home): Converte para tailwind e add version desktop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,25 @@
 import "../components/Home.css"
-import home from "../images/home.jpg"
+import home from "../images/Infomation.avif"
 
 function Home() {
-    return(
-        <div className="Home">
-            <h1>Conference on <span>Figma</span>, Creative Design, and Artificial Inteligence!</h1>
-            <div className="Card">
-                <h2><span>O</span> Where? When? What time?</h2>
-                <p><span>Kyiv, Palace of Culture</span></p>
-                <div className="date">
-                    <span><p>15-16.04.2024</p></span>
-                    <span><p>Start at 10:00</p></span>
+    return (
+        <div  className="lg:flex pt-20" style={{backgroundColor:'#121c4b'}}>
+            <div className="lg:w-1/2 text-neutral-100 flex flex-col items-end">
+                <h1 className="mt-5 font-semibold text-center lg:text-center text-3xl lg:text-4xl mx-auto   lg:w-9/12  w-11/12 mb-5" >Conference on <span>Figma</span>, Creative Design, and Artificial Inteligence!</h1>
+                <div className="w-max lg:w-9/12  mx-auto bg-neutral-100/5 text-xl flex flex-col items-center space-y-5 p-10 lg:px-5 lg:py-14 rounded-xl ">
+                    <h2><span>O</span> Where? When? What time?</h2>
+                    <p><span>Kyiv, Palace of Culture</span></p>
+                    <div className="flex space-x-5">
+                        <p href="">15-16.04.2024</p>
+                        <p href="">Start at 10:00</p>
+                    </div>
+                </div>
+                <div className="flex w-full lg:w-9/12 mx-auto lg:my-10    justify-between  flex-col lg:flex-row  font-bold space-y-5 lg:space-y-0 lg:space-x-5 items-center my-5">
+                    <a className="w-11/12 lg:w-6/12 bg-pink-600 rounded-md  h-12 flex justify-center items-center" >Book tickets for 2025</a>
+                    <a className="w-11/12 lg:w-6/12 bg-teal-600 rounded-md  h-12 flex justify-center items-center" id="second">Partner with us</a>
                 </div>
             </div>
-            <img src={home} alt="" />
-            <button >Book tickets</button>
-            <button id="second">Partner with us</button>
+            <img className=" object-cover lg:w-1/2" src={home} alt="" />
         </div>
     )
 }
